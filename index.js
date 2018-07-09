7e2e605d33e1441ac4a95e6766802cc17fd9952e
arisupriatna14 <arisupriatna703@gmail.com>
2018-07-09 13:53:14 +0700

    fix typo number romawi 9

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function to_roman(num) {
   const angkaRomawi = { 
     'M': 1000, 'CM': 900, 'D': 500, 'CD': 400,   
     'C': 100, 'XC': 90, 'L': 50, 'XL': 40,   
-    'X': 10, 'XI': 9, 'V': 5, 'IV': 4, 'I': 1   
+    'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1   
   }
   let romanStr = ''
 
